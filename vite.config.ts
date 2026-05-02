591fab090b23e564af57ff0045193fa40c21d7ee
jaesung <peter.yu.0402@gmail.com>
2026-05-02 20:28:54 +0900

    feat: add workflow for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
-export default {
-  base: '/toy01-vertical-shooter/'
-}
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/toy01-vertical-shooter/', 
+  build: {
+    outDir: 'dist', // 빌드 결과물이 저장될 폴더 이름
+  }
+});
